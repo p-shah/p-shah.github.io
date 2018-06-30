20db5045dfe0454803db64a79a3d10bdc055c286
unknown <pdshah6@gmail.com>
2018-06-29 22:42:33 -0400

    Changed favicon, added Dribbble, Cleaned up App.js, Removed not good joke

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import { Well } from 'react-bootstrap';
+import IconBar from 'components/IconBar.js';
+import shuffleboard from 'images/shuffleboard.PNG';
 
 class About extends Component {
   render() {
-    return(
-      <div class="col-md-8">
-        <h1 className="page-header">Welcome. <small>Happy to have you here!</small></h1>
-        <Well className="description-card shadow">
-          <h3><b>Market Yo Self! </b><small>(That's a Parks and Recs reference, just in case you didn't get it!)</small></h3>
-          <p>I graduated from Drexel University in 2016 with a Bachelor's in Computer and Electrical Engineering. I am currently
-          pursing my Master's in Computer Science at Villanova University. As a lifelong critic of tech, I want to push the envelope
-          using data and design. In doing so, I strive to create clean and structured code with a purpose. Throughout my career so
-          far, I have had the ability to develop an UI in AngularJS, build out RESTful APIs with Spring, write bash scripts using Hadoop
-          on AWS.</p>
-        </Well>
+    return (
+      <div class="row">
+        <div class="col-md-4">
+          <img className="img-responsive shadow" src={shuffleboard} alt="me" />
+          <IconBar />
+        </div>
+        <div class="col-md-8">
+          <h1 className="page-header">
+            Welcome. <small>Happy to have you here!</small>
+          </h1>
+          <Well className="description-card shadow">
+            <h3>
+              <b>About Me.</b>
+            </h3>
+            <p>
+              I graduated from Drexel University in 2016 with a Bachelor's in
+              Computer and Electrical Engineering. I am currently pursing my
+              Master's in Computer Science at Villanova University. As a
+              lifelong critic of tech, I want to push the envelope using data
+              and design. In doing so, I strive to create clean and structured
+              code with a purpose. Throughout my career so far, I have had the
+              ability to develop an UI in AngularJS, build out RESTful APIs with
+              Spring, write bash scripts using Hadoop on AWS.
+            </p>
+          </Well>
+        </div>
       </div>
     );
   }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
 import 'containers/App.css';
-import shuffleboard from 'images/shuffleboard.PNG';
-import MyNavbar from 'components/MyNavbar.js'
-import IconBar from 'components/IconBar.js'
-import Footer from 'components/Footer.js'
-import About from 'components/About.js'
+import MyNavbar from 'components/MyNavbar.js';
+import Footer from 'components/Footer.js';
+import About from 'components/About.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MyNavbar />
-        <div class="row">
-          <div class="col-md-4">
-            <img className="img-responsive shadow" src={shuffleboard} alt="me"/>
-            <IconBar></IconBar>
-          </div>
-          <About />
-        </div>
+        <About />
         <Footer />
       </div>
     );
