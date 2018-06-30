8ddc2b17fa704def4939eaa493415905f18e343c
Priya Shah <pdshah6@gmail.com>
2018-06-29 22:42:33 -0400

    Changed favicon, added Dribbble, Cleaned up App.js, Removed not good joke

--- src/components/IconBar.js
+++ src/components/IconBar.js
@@ -2,17 +2,41 @@ import React, { Component } from 'react';
 import 'containers/App.css';
 import linkedin from 'images/linkedin.svg';
 import spotify from 'images/spotify.svg';
-import mail from 'images/envelope.svg'
-import resume from 'images/document.svg'
+import mail from 'images/envelope.svg';
+import resume from 'images/document.svg';
+import dribbble from 'images/dribbble.svg';
 
 class IconBar extends Component {
   render() {
-    return(
+    return (
       <div className="icon-block col-md-offset-2">
-        <a href="mailto:pdshah6@gmail.com"><img className="icon" src={mail} alt="gmail"/></a>
-        <a href="https://open.spotify.com/user/122818050?si=vgec4TXTQ6Cq4_2ok1HoeA" rel="noopener noreferrer" target="_blank"><img className="icon" src={spotify} alt="spotify"/></a>
-        <a href="https://www.linkedin.com/in/priya-shah-9a220949/" rel="noopener noreferrer" target="_blank"><img className="icon" src={linkedin} alt="linkedin"/></a>
-        <a href="" rel="noopener noreferrer" target="_blank"><img className="icon" src={resume} alt="resume"/></a>
+        <a href="mailto:pdshah6@gmail.com">
+          <img className="icon" src={mail} alt="gmail" />
+        </a>
+        <a
+          href="https://open.spotify.com/user/122818050?si=vgec4TXTQ6Cq4_2ok1HoeA"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img className="icon" src={spotify} alt="spotify" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/priya-shah-9a220949/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img className="icon" src={linkedin} alt="linkedin" />
+        </a>
+        <a
+          href="https://dribbble.com/pdshah"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img className="icon" src={dribbble} alt="dribbble" />
+        </a>
+        <a href="" rel="noopener noreferrer" target="_blank">
+          <img className="icon" src={resume} alt="resume" />
+        </a>
       </div>
     );
   }
