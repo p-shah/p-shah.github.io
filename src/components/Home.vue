139089e5df5e6fc4b4423426a91564264d45c1f1
Priya <pdshah6@gmail.com>
2021-11-05 10:44:01 -0400

    Add fonts, HBDI, PI & LinkedIn

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,20 +2,18 @@
     <div>
         <h1 class="hi"><span class="highlight">Hi!</span> I'm Priya</h1>
         <div class="about">
-            <p>I work as a Technology Consultant.</p>
+            <p>I am a bubbly and collaborative fast learner looking for my next project.</p>
+            <p>I currently work as a Technology Consultant.</p>
             <p>I have experience working on web, mobile and cloud infrastructure projects.</p>
-            <p>I have worked in the following industries: Pharmeceuticals, Energy, Financial Services (Credit Cards, Investments).</p>
-            <p>I presented <a href="http://edwardkim.net/files/sparsecodingdefense.pdf"> "A Neuromorphic Sparse Coding Defense to Adversarial Images"</a> -- Edward Kim, Jessica Yarnall, Priya Shah (Villanova University) and Garrett Kenyon at <a href="https://icons.ornl.gov/">ICONS</a> 2019</p>
+            <p>I have worked in the following industries: Pharmeceuticals, Distribution, Energy, Financial Services (Credit Cards, Investments).</p>
+            <p>I presented <a href="http://edwardkim.net/files/sparsecodingdefense.pdf"> "A Neuromorphic Sparse Coding Defense to Adversarial Images"</a> at <a href="https://icons.ornl.gov/">ICONS</a> 2019.</p>
             <p>I completed my Master's in Computer Science at Villanova and my Bachelor's in Electrical and Computer Engineering at Drexel.</p>
-            <!-- <p>Here is my resume.</p> -->
-            <p>Feel free to <a href="mailto:pdshah6@gmail.com">email me.</a></p>
+            <p>I am a <a href="./PI.pdf" target="_blank">Promoter</a> on the PI Index and my HBDI Pattern is <a href="./HBDI.pdf" target="_blank">2211</a>.</p>
+            <p><a href="./Resume.pdf" target="_blank">Here</a> is my resume.</p>
+            <p>Feel free to <a href="mailto:pdshah6@gmail.com">email me</a> or add me on <a href="https://www.linkedin.com/in/priya-shah-9a220949/" target="_blank">LinkedIn.</a></p>
         </div>
-        <!-- <p>I consume a lot of media: podcasts, books, TV, movies and music!</p>
-        <p>I love fruit stands on the side of the street.</p>
-        <p>I enjoy cooking.</p> -->
     </div>
 </template>
-
 <style scoped>
     h1 
     {
@@ -27,6 +25,15 @@
     .highlight 
     {
         color: #71c272;
+    }
+
+    h1 {
+        font-family: "Zen Antique";
+    }
+    
+    p {
+        font-family: "Noto Sans";
+        font-size: 16px;
     }
 
     .about {
